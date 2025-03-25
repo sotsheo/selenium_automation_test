48822151cd2ddb1de81459860e3d08035d25f9ab
ThanhNV <thanh.van.nguyen@logigear.com>
2025-03-25 10:02:39 +0700

    test by test case

--- python/main.py
+++ python/main.py
@@ -9,25 +9,34 @@ class TestLogin(unittest.TestCase):
     def setUp(self):
         # Khởi tạo ChromeDriver
         self.driver = webdriver.Chrome()
-        self.driver.get("https://shop-new-dev.mvno.help/")  # Thay URL của bạn vào đây
+        self.driver.get("https://mypage-new-dev.mvno.help/")  # Thay URL của bạn vào đây
 
     def test_login_success(self):
+        # test hien thi
+        print("Test hien thi")
+        time.sleep(2)
+        self.is_displayed('box-login-logo', 'logo', By.CLASS_NAME)
+        self.is_displayed('toggle-menu', 'icon menu', By.CLASS_NAME)
+        self.get_compare_text('box-copyright', 'Copyright © 2025 LEQUIOS Co., Ltd. All Rights Reserved.', 'copyright', True)
+        self.is_displayed('login_id', 'input login', By.NAME)
+        self.is_displayed('password', 'input password', By.NAME)
+        self.is_displayed('btn-bg-blue', 'input submit', By.CLASS_NAME)
         # case required 
-        self.submit_login()
-        time.sleep(2) 
-        print("Case: Loi required all")
-        self.get_error_input("message-agency_cd", "代理店コードを入力してください。", "Loi required")
-        self.get_error_input("message-user_id", "ログインIDを入力してください。", "Loi required")
-        self.get_error_input("message-password", "パスワードを入力してください。", "Loi required")
+        # self.submit_login()
+        # time.sleep(2) 
+        # print("Case: Loi required all")
+        # self.get_error_input("message-agency_cd", "代理店コードを入力してください。", "Loi required")
+        # self.get_error_input("message-user_id", "ログインIDを入力してください。", "Loi required")
+        # self.get_error_input("message-password", "パスワードを入力してください。", "Loi required")
 
-        print("Case: Loi required agency_cd")
-        self.enter_input("password", "password")
-        self.enter_input("user_id", "userid")
-        self.submit_login()
-        time.sleep(2) 
-        self.get_error_input("message-agency_cd", "代理店コードを入力してください。", "Loi required")
-        self.get_error_input("message-user_id", "s", "user_id Khong loi")
-        self.get_error_input("message-password", "",  "password Khong loi")
+        # print("Case: Loi required agency_cd")
+        # self.enter_input("password", "password")
+        # self.enter_input("user_id", "userid")
+        # self.submit_login()
+        # time.sleep(2) 
+        # self.get_error_input("message-agency_cd", "代理店コードを入力してください。", "Loi required")
+        # self.get_error_input("message-user_id", "s", "user_id Khong loi")
+        # self.get_error_input("message-password", "",  "password Khong loi")
         # Step 1: Nhập mã agency_cd
         # self.enter_input("agency_cd", "")
         # self.enter_input("user_id", "")
@@ -38,7 +47,19 @@ class TestLogin(unittest.TestCase):
 
         # Step 5: Kiểm tra thông báo lỗi nếu có
         # self.check_error_message("Đã có lỗi xảy ra.")
-        
+    
+    def is_displayed(self, name, target, by): 
+        try:
+            logo = self.driver.find_element(by, name)  # Thay "logo" bằng ID thực tế của logo
+            
+            # Kiểm tra xem logo có hiển thị không
+            if logo.is_displayed():
+                print(f"✅ {target} hiển thị đúng.")
+            else:
+                print(f"❌ {target} không hiển thị.")
+        except:
+            print(f"❌ Không tìm thấy {target} trên trang.")
+
     def enter_input(self, name, value):
         agency_cd_input = self.driver.find_element(By.NAME, name)
         agency_cd_input.send_keys(value)
@@ -47,13 +68,13 @@ class TestLogin(unittest.TestCase):
         password_input = self.driver.find_element(By.ID, "btnLogin")
         password_input.send_keys(Keys.RETURN)
 
-    def get_error_input(self, name, expected_error, case):
-        error_div = self.driver.find_element(By.ID, name)
+    def get_compare_text(self, name, expected_error, case, className = False):
+        error_div = self.driver.find_element(By.CLASS_NAME if className else By.ID, name)
         print(f" - Input: {case}")
         print(f"   - Ket qua mong muon: {expected_error}")
         try:
             self.assertEqual(error_div.text, expected_error)
-            print("\033[94m    => Thành công! Thông báo lỗi khớp!\033[0m")
+            print("\033[94m    => Thành công! Thông báo khớp!\033[0m")
         except Exception as e:
             print(f"\033[31m    => Lỗi tra ve: {error_div.text} \033[0m")
 
